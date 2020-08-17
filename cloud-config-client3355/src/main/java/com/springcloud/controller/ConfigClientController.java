d359a257645cd59fa335efb365ab685490f297e9
十七 <chenaichenet@126.com>
2020-08-17 10:01:42 +0800

    Config分布式配置中心

--- cloud-config-client3355/src/main/java/com/springcloud/controller/ConfigClientController.java
+++ cloud-config-client3355/src/main/java/com/springcloud/controller/ConfigClientController.java
@@ -13,12 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ConfigClientController {
+    @Value("${server.prot}")
+    private String serverPort;
+
     //Config可以将配置信息以Rest接口的形式暴露
     @Value("${config.info}")
     private String configInfo;
 
     @GetMapping("/config/configInfo")
     public String getConfigInfo(){
-        return configInfo;
+        return "ServerPort: "+serverPort+"\t\nConfigInfo: "+configInfo;
     }
 }
